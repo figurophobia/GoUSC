c6c01f3ac2fd6bc5d1c2bcd1b4a268d635d8b30f
DavidSM <dasamiarea@gmail.com>
2026-09-16 10:46:52 +0200

    Initial commit: Spring Boot GoUSC project

---
+++ src/main/java/com/usc/boardgames/gousc/GoUscApplication.java
@@ -0,0 +1,13 @@
+package com.usc.boardgames.gousc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoUscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoUscApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/usc/boardgames/gousc/GoUscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usc.boardgames.gousc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoUscApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
